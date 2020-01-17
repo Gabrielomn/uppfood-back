4405e16936d7f4de65a9a8ce4862887d0fb67431
kaladin <gabriel.nobrega@ccc.ufcg.edu.br>
2020-01-17 16:43:53 -0300

    add cors

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ dotenv.config()
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8080
+const cors = require('cors')
+app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, access-control-allow-origin");
+    next();
+});
 require('./router')(app)
 app.listen (PORT, () => {
     console.log(`App is listening on port ${PORT}`)
