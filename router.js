d69dd4574dc6d7ffec0243e8d6f85c3389e1cbce
kaladin <gabriel.nobrega@ccc.ufcg.edu.br>
2020-01-21 22:58:57 -0300

    add swagger

--- router.js
+++ router.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const food = require('./services/food/food.router')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./docs/swagger.json')
+
+
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.use('/food', food)
 module.exports =  (app) => {
     app.use('/', router)
